dfbda44811402f7021bbdc62600f188b78ed9531
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2023-08-11 11:39:47 +0330

    Update Airline_flights_identification.py

--- Phase2/Airline_flights_identification.py
+++ Phase2/Airline_flights_identification.py
@@ -1,1 +1,14 @@
+# Define our main tools:
+from FlightRadar24.api import FlightRadar24API
+fr_api = FlightRadar24API()
 
+# Definition of tools used in reading images
+from PIL import Image
+import requests
+from io import BytesIO
+
+# This library will be used to display images
+from matplotlib import pyplot as plt
+
+# Map tool
+import folium

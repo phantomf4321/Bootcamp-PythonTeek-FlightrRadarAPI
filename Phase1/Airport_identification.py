bec667431fab5f1bbc956302216f5aecac357c9f
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2023-08-14 12:00:37 +0330

    Update Airport_identification.py
    complete step2

--- Phase1/Airport_identification.py
+++ Phase1/Airport_identification.py
@@ -32,12 +32,10 @@ city = position['region']['city']
 print("{} in: {} / {}".format(name, city, country))
 
 
-'''
-Step2:
-latitude = ...
-longitude = ...
-elevation = ...
-'''
+# Step2:
+latitude = position['latitude']
+longitude = position['longitude']
+elevation = position['elevation']
 
 '''
 Step3:
